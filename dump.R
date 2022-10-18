7007f08547c4aa14912c9e542669c0aeeb2e0bd3
wafi0 <17b2200@ubd.edu.bn>
2022-10-18 14:38:23 +0800

    Edit dump script

--- dump.R
+++ dump.R
@@ -1,4 +1,20 @@
 library(ggplot2)
 library(palmerpenguins)
-library()
-penguins %>% print(n = 5)
+library(dplyr)
+ggplot(
+  data = penguins,
+  mapping = aes(x = bill_depth_mm,
+                y = bill_length_mm)
+) +
+  geom_point(
+    mapping = aes(colour = species)
+  ) +
+  labs(title = "Bill depth and length",
+       subtitle = paste("Dimensions for Adelie,",
+                        "Chinstrap, and Gentoo",
+                        "Penguins"),
+       x = "Bill depth (mm)",
+       y = "Bill length (mm)",
+       colour = "Species",
+       caption = "Source: palmerpenguins package"
+  )
